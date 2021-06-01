f340f3347227826c8efc75220f7af5f99b59a3dc
Yunus İNAN <yns.inan1660@gmail.com>
2021-06-01 12:56:22 +0300

    Responsive

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navigation from "./navigation"
 import styles from '../styles/layout.module.scss'
-import Footer from "./Footer"
+
 export default function Layout({ children }) {
     return (
         <div className={styles.layout}>
             <Navigation />
 
             <div className={styles.children}>{children}</div>
-            
+
         </div>
 
     )
